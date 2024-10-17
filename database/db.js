2de540f4fd013fb48d04813e0c46916c5e294367
Hieu Nguyen <hieu.nv2001hust@gmail.com>
2024-10-17 16:21:01 +0700

    mongoAtlas

--- database/db.js
+++ database/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/ecohealth', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    await mongoose.connect(
+      'mongodb+srv://kz01123008:1@mymongodb.33jb4.mongodb.net/?retryWrites=true&w=majority&appName=mymongodb',
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    console.log('MongoDB connected to Atlas');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB Atlas:', error);
     process.exit(1);
   }
 };
