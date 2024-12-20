061902777062c33a5c457da18019e0c2f6d47643
unknown <hieu.nv2001hust@gmail.com>
2024-12-20 14:59:52 +0700

    fix2

--- index.js
+++ index.js
@@ -151,104 +151,198 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/devices", deviceRoutes);
 
-// Khởi tạo MQTT client
-const mqttClient = mqtt.connect('mqtt://103.1.238.175', {
-  port: 1883,
-  username: 'test',
-  password: 'testadmin'
-});
+// // Khởi tạo MQTT client
+// const mqttClient = mqtt.connect('mqtt://103.1.238.175', {
+//   port: 1883,
+//   username: 'test',
+//   password: 'testadmin'
+// });
 
-// Kết nối và subscribe tới topic 'device'
-mqttClient.on('connect', () => {
-  console.log('Connected to MQTT broker');
-  const topic = 'device'; // Đặt tên topic là 'device'
-  mqttClient.subscribe(topic, (err) => {
-    if (err) {
-      console.log('Subscription failed:', err);
-    } else {
-      console.log(`Subscribed to topic: ${topic}`);
-    }
-  });
-});
+// // Kết nối và subscribe tới topic 'device'
+// mqttClient.on('connect', () => {
+//   console.log('Connected to MQTT broker');
+//   const topic = 'device'; // Đặt tên topic là 'device'
+//   mqttClient.subscribe(topic, (err) => {
+//     if (err) {
+//       console.log('Subscription failed:', err);
+//     } else {
+//       console.log(`Subscribed to topic: ${topic}`);
+//     }
+//   });
+// });
 
-// Nhận dữ liệu từ topic 'device' và xử lý
-mqttClient.on('message', async (topic, message) => {
-  try {
-    const data = JSON.parse(message.toString());
-    console.log(`Data received from topic ${topic}:`, data);
+// // Nhận dữ liệu từ topic 'device' và xử lý
+// mqttClient.on('message', async (topic, message) => {
+//   try {
+//     const data = JSON.parse(message.toString());
+//     console.log(`Data received from topic ${topic}:`, data);
 
-    if (!data || Object.keys(data).length === 0) {
-      console.log("Received empty data");
-      return;
-    }
+//     if (!data || Object.keys(data).length === 0) {
+//       console.log("Received empty data");
+//       return;
+//     }
 
-    for (const deviceName of Object.keys(data)) {
-      const deviceData = data[deviceName];
-      const device = await Device.findOne({ deviceId: deviceName });
+//     for (const deviceName of Object.keys(data)) {
+//       const deviceData = data[deviceName];
+//       const device = await Device.findOne({ deviceId: deviceName });
 
-      if (device) {
-        device.sensorData.push(deviceData);
-        device.lastChecked = Date.now();
-        await device.save();
-        console.log(`Updated sensorData for device ${deviceName}`);
-      } else {
-        console.log(`Device with deviceId ${deviceName} not found`);
-      }
+//       if (device) {
+//         device.sensorData.push(deviceData);
+//         device.lastChecked = Date.now();
+//         await device.save();
+//         console.log(`Updated sensorData for device ${deviceName}`);
+//       } else {
+//         console.log(`Device with deviceId ${deviceName} not found`);
+//       }
 
-      let responseData = {
-        gas_ppm: "normal",
-        flame_detected: "normal",
-        temperature: "normal",
-        humidity: "normal",
-        dust_density: "normal"
-      };
-      let warning = false;
+//       let responseData = {
+//         gas_ppm: "normal",
+//         flame_detected: "normal",
+//         temperature: "normal",
+//         humidity: "normal",
+//         dust_density: "normal"
+//       };
+//       let warning = false;
 
-      if (deviceData.gas_ppm > 2000) {
-        responseData.gas_ppm = "warning";
-        warning = true;
-      }
+//       if (deviceData.gas_ppm > 2000) {
+//         responseData.gas_ppm = "warning";
+//         warning = true;
+//       }
 
-      if (deviceData.flame_detected === 0) {
-        responseData.flame_detected = "warning";
-        warning = true;
-      }
+//       if (deviceData.flame_detected === 0) {
+//         responseData.flame_detected = "warning";
+//         warning = true;
+//       }
 
-      if (deviceData.temperature > 100) {
-        responseData.temperature = "warning";
-        warning = true;
-      }
+//       if (deviceData.temperature > 100) {
+//         responseData.temperature = "warning";
+//         warning = true;
+//       }
 
-      if (deviceData.humidity < 30) {
-        responseData.humidity = "warning";
-        warning = true;
-      }
+//       if (deviceData.humidity < 30) {
+//         responseData.humidity = "warning";
+//         warning = true;
+//       }
 
-      if (deviceData.dust_density > 1000) {
-        responseData.dust_density = "warning";
-        warning = true;
-      }
+//       if (deviceData.dust_density > 1000) {
+//         responseData.dust_density = "warning";
+//         warning = true;
+//       }
 
-      console.log("Response Data:", responseData);
+//       console.log("Response Data:", responseData);
 
-      // Gửi phản hồi lên topic mới dựa vào tên thiết bị
-      const responseTopic = `response/${deviceName}`;
-      mqttClient.publish(responseTopic, JSON.stringify(responseData), (err) => {
-        if (err) {
-          console.log('Error sending data:', err);
-        } else {
-          console.log(`Sent data to topic ${responseTopic}`);
-        }
-      });
-    }
+//       // Gửi phản hồi lên topic mới dựa vào tên thiết bị
+//       const responseTopic = `response/${deviceName}`;
+//       mqttClient.publish(responseTopic, JSON.stringify(responseData), (err) => {
+//         if (err) {
+//           console.log('Error sending data:', err);
+//         } else {
+//           console.log(`Sent data to topic ${responseTopic}`);
+//         }
+//       });
+//     }
 
-  } catch (error) {
-    console.error('Error parsing message:', error);
-  }
-});
+//   } catch (error) {
+//     console.error('Error parsing message:', error);
+//   }
+// });
 
 
 // Khởi động server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  const mqttClient = mqtt.connect('mqtt://103.1.238.175', {
+    port: 1883,
+    username: 'test',
+    password: 'testadmin'
+  });
+  
+  // Kết nối và subscribe tới topic 'device'
+  mqttClient.on('connect', () => {
+    console.log('Connected to MQTT broker');
+    const topic = 'device'; // Đặt tên topic là 'device'
+    mqttClient.subscribe(topic, (err) => {
+      if (err) {
+        console.log('Subscription failed:', err);
+      } else {
+        console.log(`Subscribed to topic: ${topic}`);
+      }
+    });
+  });
+  
+  // Nhận dữ liệu từ topic 'device' và xử lý
+  mqttClient.on('message', async (topic, message) => {
+    try {
+      const data = JSON.parse(message.toString());
+      console.log(`Data received from topic ${topic}:`, data);
+  
+      if (!data || Object.keys(data).length === 0) {
+        console.log("Received empty data");
+        return;
+      }
+  
+      for (const deviceName of Object.keys(data)) {
+        const deviceData = data[deviceName];
+        const device = await Device.findOne({ deviceId: deviceName });
+  
+        if (device) {
+          device.sensorData.push(deviceData);
+          device.lastChecked = Date.now();
+          await device.save();
+          console.log(`Updated sensorData for device ${deviceName}`);
+        } else {
+          console.log(`Device with deviceId ${deviceName} not found`);
+        }
+  
+        let responseData = {
+          gas_ppm: "normal",
+          flame_detected: "normal",
+          temperature: "normal",
+          humidity: "normal",
+          dust_density: "normal"
+        };
+        let warning = false;
+  
+        if (deviceData.gas_ppm > 2000) {
+          responseData.gas_ppm = "warning";
+          warning = true;
+        }
+  
+        if (deviceData.flame_detected === 0) {
+          responseData.flame_detected = "warning";
+          warning = true;
+        }
+  
+        if (deviceData.temperature > 100) {
+          responseData.temperature = "warning";
+          warning = true;
+        }
+  
+        if (deviceData.humidity < 30) {
+          responseData.humidity = "warning";
+          warning = true;
+        }
+  
+        if (deviceData.dust_density > 1000) {
+          responseData.dust_density = "warning";
+          warning = true;
+        }
+  
+        console.log("Response Data:", responseData);
+  
+        // Gửi phản hồi lên topic mới dựa vào tên thiết bị
+        const responseTopic = `response/${deviceName}`;
+        mqttClient.publish(responseTopic, JSON.stringify(responseData), (err) => {
+          if (err) {
+            console.log('Error sending data:', err);
+          } else {
+            console.log(`Sent data to topic ${responseTopic}`);
+          }
+        });
+      }
+  
+    } catch (error) {
+      console.error('Error parsing message:', error);
+    }
+  });
 });
